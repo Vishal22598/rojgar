8ef688d0bee3a6eed00acebb71fac51f511acdf5
Vishal22598 <vishalgupta22598@gmail.com>
2026-01-15 23:02:52 +0530

    application-service spring app is created to store the data of every job applied

---
+++ application-service/src/test/java/com/rojgar/applicationService/ApplicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rojgar.applicationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
