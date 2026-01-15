755255d6872d57f9ff7ff4d6b5ffa83a422e6e3a
Vishal22598 <vishalgupta22598@gmail.com>
2026-01-15 17:38:13 +0530

    job-service application created

---
+++ job-service/src/main/java/com/rojgar/jobService/JobServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rojgar.jobService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobServiceApplication.class, args);
+	}
+
+}
